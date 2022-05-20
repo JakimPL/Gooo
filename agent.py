915143004376f596f647e4d0b707874680a5034a
JakimPL <jakimpl@gmail.com>
2022-05-20 19:25:35 +0200

    Model OpenSpiel

--- agent.py
+++ agent.py
@@ -1,66 +1,21 @@
-import torch
-import random
 import numpy as np
-from collections import deque
-from state import State
-from model import LQN, QTRN
-from config import Config
-
-config = Config()
+from open_spiel.python.algorithms import mcts
+from open_spiel.python.algorithms.alpha_zero import evaluator as az_evaluator
+from open_spiel.python.algorithms.alpha_zero import model as az_model
 
 
-class Agent:
-    def __init__(self, n, learning_rate=config.alpha, gamma=config.gamma, hidden_layers=config.layers,
-                 max_len=config.max_len, alpha=config.alpha, batch_size=config.batch_size, power=config.power):
-        self.power = power
-        self.size = n
-        self.batch_size = batch_size
-        self.learning_rate = learning_rate
-        self.games = 0
-        self.eps = 0
-        self.gamma = gamma
-        self.memory = deque(maxlen=max_len)
-        self.model = LQN(2 * self.size + 1, hidden_layers, self.size)
-        self.trainer = QTRN(self.model, alpha, gamma=self.gamma)
-
-    @staticmethod
-    def get_state(state: State):
-        return np.array([state.player] + state.positions[0] + state.positions[1], dtype=int)
-
-    def remember(self, state, move, reward, next_state, end):
-        self.memory.append((state, move, reward, next_state, end))
-
-    def train_short(self, current_state, move, reward, next_state, end):
-        self.trainer.train_step(current_state, move, reward, next_state, end)
-
-    def train_long(self):
-        sample = random.sample(self.memory, self.batch_size) if len(self.memory) > self.batch_size else self.memory
-
-        current_state, move, reward, next_state, end = zip(*sample)
-        self.trainer.train_step(current_state, move, reward, next_state, end)
-
-    def get_prediction_vector(self, state: np.array):
-        return self.model(torch.tensor(state, dtype=torch.float))
-
-    def get_prediction(self, state: np.array):
-        return torch.argmax(self.get_prediction_vector(state)).item()
-
-    def get_action(self, state: np.array):
-        game_state = self.state_from_array(state)
-        self.eps = 1 / ((self.games + 1) ** self.power)
-        move = [0] * self.size
-        if random.random() < self.eps:
-            index = random.choice(game_state.get_possible_moves())
-        else:
-            index = self.get_prediction(state)
-
-        move[index] = 1
-        return move
-
-    @staticmethod
-    def state_from_array(array: np.array) -> State:
-        size = (len(array) - 1) // 2
-        state = State(size)
-        state.player = array[0]
-        state.positions = [array[1:size + 1], array[size + 1:]]
-        return state
+def get_bot(game, size: int) -> mcts.MCTSBot:
+    rng = np.random.RandomState()
+    model_path = 'model_{size}x{size}/checkpoint--1'.format(size=size)
+    model = az_model.Model.from_checkpoint(model_path)
+    evaluator = az_evaluator.AlphaZeroEvaluator(game, model)
+    return mcts.MCTSBot(
+        game,
+        2,
+        1000,
+        evaluator,
+        random_state=rng,
+        child_selection_fn=mcts.SearchNode.puct_value,
+        solve=True,
+        verbose=False
+    )
--- game.py
+++ game.py
@@ -1,53 +1,75 @@
-import pygame
 import sys
-from state import State
+
+import pygame
+import pyspiel
 from pygame import locals
+
+from agent import get_bot
+from state import State
 from ui import UI
-from trainer import Trainer
 
 
 class Game:
-    def __init__(self, n=3, autoplay=False):
+    def __init__(self, board_size: int = 5, autoplay: tuple[bool, bool] = (False, False)):
         pygame.init()
         self.autoplay = autoplay
-        self.n = n
+        self.board_size = board_size
         self.FPS = 60
-        self.state = State(n)
-        self.ui = UI(self.n + 1)
+        self.state = State(self.board_size)
+        self.ui = UI(self.board_size + 1)
         self.clock = pygame.time.Clock()
-        self.trainer = Trainer(n)
-        self.trainer.load()
+
+        self.game: pyspiel.Game = pyspiel.load_game('gooo')
+        self.az_state: pyspiel.State = self.game.new_initial_state()
+        self.bot = get_bot(self.game, self.board_size)
+        self.suggested_action = self._get_suggested_action()
+
+        self.element = None
+        self.position = None
+
+    def _get_suggested_action(self):
+        return self.bot.step(self.az_state)
+
+    def _get_events(self):
+        if not self.state.end and pygame.mouse.get_pressed(3)[0]:
+            x, y = self.ui.get_mouse_positions(pygame.mouse.get_pos())
+            self.element = (x if self.state.player else y) - 1
+            self.position = y if self.state.player else x
+
+        for event in pygame.event.get():
+            if event.type == locals.QUIT:
+                pygame.quit()
+                sys.exit()
+
+    def _make_move(self, element: int):
+        if self.state.is_move_possible(element):
+            self.state.move(element)
+            self.az_state.apply_action(element)
+            if not self.az_state.is_terminal():
+                self.suggested_action = self.bot.step(self.az_state)
 
     def frame(self):
         self.ui.draw_game(self.state)
-        self.ui.draw_text("Game {0}. {1} player turn.".format(self.trainer.agent.games + 1, "Red" if self.state.player else "Blue"))
-        if self.autoplay:
-            self.state = self.trainer.state
-            self.trainer.train_step()
-        else:
-            if not self.state.end and pygame.mouse.get_pressed(3)[0]:
-                x, y = self.ui.get_mouse_positions(pygame.mouse.get_pos())
-                element = (x if self.state.player else y) - 1
-                position = y if self.state.player else x
-                if 0 <= element < self.n and self.state.positions[self.state.player][element] == position and self.state.is_move_possible(element):
-                    self.state.move(element)
-
+        self.ui.draw_text("{turn} player turn.".format(turn="Red" if self.state.player else "Blue"))
         if self.state.points != 0:
-            text = "{2} player wins by {0} point{1}.".format(self.state.points, "s" if self.state.points > 1 else "", "Red" if self.state.points > 0 else "Blue")
+            text = "{player} player wins by {points} point{plural}.".format(
+                points=abs(self.state.points),
+                plural="s" if abs(self.state.points) > 1 else "",
+                player="Red" if self.state.points > 0 else "Blue"
+            )
         else:
             text = "Draw."
 
         self.ui.draw_text(text)
+        if self.suggested_action is not None:
+            self.ui.draw_text("Suggested action: {action}.".format(action=self.suggested_action))
 
-        np_state = self.trainer.agent.get_state(self.state)
-        prediction_vector = [round(i.item(), 6) for i in self.trainer.agent.get_prediction_vector(np_state)]
-        best_move = self.trainer.agent.get_prediction(np_state)
-        self.ui.draw_text("{0} (best move: {1})".format(prediction_vector, best_move))
+        self._get_events()
+        if self.autoplay[self.state.player]:
+            self._make_move(self.suggested_action)
+        else:
+            if self.element is not None and 0 <= self.element < self.board_size and self.state.positions[self.state.player][self.element] == self.position:
+                self._make_move(self.element)
 
-        for event in pygame.event.get():
-            if event.type == locals.QUIT:
-                self.trainer.agent.model.save()
-                pygame.quit()
-                sys.exit()
         self.clock.tick(self.FPS)
         pygame.display.update()
